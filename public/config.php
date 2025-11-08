30c4b7b8b874168915f08b40722befa529326039
Luke Duffy <luke.duffy@duffit.com>
2025-11-08 18:32:27 +1100

    Deploy static pages plugin with clean URL support
    - Add local_staticpages plugin to Moodle public directory
    - Create /pages/ directory with URL rewriting support
    - Configure .htaccess for both clean URLs (/pages/about) and query parameters
    - Add symlink-compatible config.php resolution
    - Enable static pages at /pages/{slug} URLs

--- public/config.php
+++ public/config.php
@@ -1,31 +1,30 @@
-<?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+<?php  // Moodle configuration file
 
-/**
- * Moodle configuration loader.
- *
- * @package    core
- * @copyright  2024 Andrew Lyons <andrew@nicols.co.uk>
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
 
-$configfile = __DIR__ . '/../config.php';
-if (!file_exists($configfile)) {
-    header("Location: install.php");
-    die;
-}
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'db';
+$CFG->dbname    = 'moodle';
+$CFG->dbuser    = 'moodle';
+$CFG->dbpass    = 'moodle';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_0900_ai_ci',
+);
 
-require($configfile);
+$CFG->wwwroot   = 'http://localhost:8080';
+$CFG->dataroot  = '/var/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 02777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
